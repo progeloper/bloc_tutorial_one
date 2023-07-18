e883948c0c32ef820caa51b1779f8bef4df7120e
Mayeavx <mayoodukoya@gmail.com>
2023-07-18 18:09:16 +0100

    combined the BlocListener and BlocBuilder into a BlocConsumer

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -5,14 +5,14 @@ part 'counter_state.dart';
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit()
       : super(
-          CounterState(counterValue: 0),
+          CounterState(counterValue: 0, wasIncremented: false),
         );
 
   void increment() => emit(
-        CounterState(counterValue: state.counterValue + 1),
+        CounterState(counterValue: state.counterValue + 1, wasIncremented: true),
       );
 
   void decrement() => emit(
-        CounterState(counterValue: state.counterValue - 1),
+        CounterState(counterValue: state.counterValue - 1, wasIncremented: false),
       );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,8 @@ class MyApp extends StatelessWidget {
     return BlocProvider(
       create: (context) => CounterCubit(),
       child: MaterialApp(
-        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        title: 'BLoC Demo',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
@@ -34,29 +35,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Center(
-
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
             ),
-            BlocBuilder<CounterCubit, CounterState>(
-              builder: (context, state) {
-                return Text(
-                  state.counterValue.toString(),
-                  style: Theme
-                      .of(context)
-                      .textTheme
-                      .headlineMedium,
-                );
+            BlocConsumer<CounterCubit, CounterState>(
+                builder: (context, state) {
+                  return Text(
+                    state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                },
+              listener: (context, state) {
+                if (state.wasIncremented) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('incremented'),
+                      duration: Duration(milliseconds: 500),
+                    ),
+                  );
+                } else if (!state.wasIncremented) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('decremented'),
+                      duration: Duration(milliseconds: 500),
+                    ),
+                  );
+                }
               },
             ),
             Row(
@@ -82,7 +93,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-
     );
   }
 }
